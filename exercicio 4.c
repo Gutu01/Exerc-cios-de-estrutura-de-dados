f4441ab848ceb5fcc8df58359c8316ef08f6dace
Gutu01 <gustavomt058@gmail.com>
2025-08-26 23:36:18 -0400

    Terminei o código

---
+++ exercicio 4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#define max_letras_produto 100
+
+typedef struct {
+
+    char nome[max_letras_produto];
+    float preco;
+    int quantidade_estoque;
+    float quantidade_estoque_float;
+    float valor_total_estoque;
+
+}Produto;
+
+void limparbufferentrada(){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int main(){
+
+    Produto produto;
+
+    printf("Nome:");
+    fgets(produto.nome, max_letras_produto, stdin);
+    produto.nome[strcspn(produto.nome, "\n")] = '\0';
+
+    printf("Preço:");
+    scanf("%f", &produto.preco);
+    limparbufferentrada();
+
+    printf("Quantidade de estoque:");
+    scanf("%d", &produto.quantidade_estoque);
+    limparbufferentrada();
+
+    produto.valor_total_estoque = produto.preco*produto.quantidade_estoque;
+
+    printf("\nValor total do estoque:R$%.2f", produto.valor_total_estoque);
+
+    return 0;
+}
